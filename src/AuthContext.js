023c0de98ef58cf979133f9dec6f6e00632257eb
Nofal Nadeem <nofalnadeem@192.168.1.12>
2024-12-01 23:17:54 +0500

    auth context finalise

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,31 +1,17 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useContext } from "react";
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    // Check localStorage for existing login state
-    const storedAuthState = localStorage.getItem("isAuthenticated");
-    if (storedAuthState === "true") {
-      setIsAuthenticated(true);
-    }
-  }, []);
-
-  const login = () => {
-    setIsAuthenticated(true);
-    localStorage.setItem("isAuthenticated", "true");
-  };
-
-  const logout = () => {
-    setIsAuthenticated(false);
-    localStorage.removeItem("isAuthenticated");
-  };
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
+
+export const useAuth = () => useContext(AuthContext);
+
+
